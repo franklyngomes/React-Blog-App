b370f0fc79ba0e53dbafed1329a68a75626f505e
franklyngomes <franklyngomes15@gmail.com>
2024-12-05 12:10:55 +0530

    Blog Details done

--- src/pages/blogs/blogDetails/BlogDetails.jsx
+++ src/pages/blogs/blogDetails/BlogDetails.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 
 const BlogDetails = () => {
   let {id} = useParams();
   console.log(id, "id")
+  const fetchData = async () => {
+    
+  }
+  useEffect(() => {
+  },[])
   return (
     <div>
       {id === }
